277e9361d03c1e7e55e343f93979afa8e814fadc
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-01-28 11:30:14 +0200

    reverted strings

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("input");
 
+        reverseStrings(sc);
+    }
 
+    private static void reverseStrings(Scanner sc) {
+        StringBuilder output = new StringBuilder();
+        String command = sc.nextLine();
+
+        while (!command.equals("end")) {
+            String reverted = new StringBuffer(command).reverse().toString();
+            output.append(command).append(" = ").append(reverted).append('\n');
+            command = sc.nextLine();
+        }
+
+        System.out.println(output);
     }
 }
