f4791caebb30d235e538dc2d866b02274cff9c87
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-01-28 12:37:45 +0200

    separated digits letters and chars

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -10,8 +12,40 @@ public class Main {
 //        reverseStrings(sc);
 //        strRepeater(sc);
 //        substrings(sc);
-        textFilter(sc);
+//        textFilter(sc);
+        separateDigits(sc);
 
+    }
+
+    private static void separateDigits(Scanner sc) {
+        String input = sc.nextLine();
+
+        // solution one
+        // System.out.println(input.replaceAll("[^\\d]", ""));
+        // System.out.println(input.replaceAll("[^a-zA-Z]", ""));
+        // System.out.println(input.replaceAll("\\w", ""));
+
+        ArrayList<Character> digits = new ArrayList<>();
+        ArrayList<Character> letters = new ArrayList<>();
+        ArrayList<Character> chars = new ArrayList<>();
+
+        for (char item : input.toCharArray()) {
+            if (Character.isDigit(item)) {
+                digits.add(item);
+                continue;
+            }
+
+            if (Character.isAlphabetic(item)) {
+                letters.add(item);
+                continue;
+            }
+
+            chars.add(item);
+        }
+
+        System.out.println(digits.stream().map(c -> Character.toString(c)).collect(Collectors.joining("")));
+        System.out.println(letters.stream().map(c -> Character.toString(c)).collect(Collectors.joining("")));
+        System.out.println(chars.stream().map(c -> Character.toString(c)).collect(Collectors.joining("")));
     }
 
     private static void textFilter(Scanner sc) {
